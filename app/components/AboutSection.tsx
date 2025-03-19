dd56fe33fd6755466750c9c11f2ef210193f7557
MuhammadShameel <mshameelkz@gmail.com>
2025-03-20 02:15:02 +0500

    about page started

---
+++ app/components/AboutSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <div>
+      <hr className="mx-auto w-full max-w-screen-xl mt-10 secondary-color" />
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
