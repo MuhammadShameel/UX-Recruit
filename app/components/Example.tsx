803278e72cf87062fbe36cfcdc096501ec070fbd
MuhammadShameel <mshameelkz@gmail.com>
2025-03-22 21:05:23 +0500

    hero section set boxes

--- app/components/Example.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Example = () => {
-  return (
-    <div className='lg:block sm:hidden text-[#ffff]'></div>
-  )
-}
-
-export default Example
---
+++ app/components/Projects.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div>
+      <hr className="mx-auto w-full max-w-screen-xl mt-10 secondary-color" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+        consequuntur soluta vero dolore eos est placeat, architecto modi
+        accusantium asperiores aliquam reprehenderit repellat inventore
+        consectetur fugit sed incidunt laudantium voluptas?
+      </p>
+    </div>
+  );
+};
+
+export default Projects;
